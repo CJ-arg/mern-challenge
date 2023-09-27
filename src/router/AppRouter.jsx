487cf91c782e14cf39f570edf51b65499f4ceec8
unknown <janoncarlos@gmail.com>
2023-09-27 17:42:27 -0300

    router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import { AuthRoutes } from '../auth/routes/AuthRoutes'
+import { TasksRoutes } from '../tasks/routes/TasksRoutes'
 
 export const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <Routes>
+      <Route path='/auth/*' element={<AuthRoutes />} />
+
+      <Route path='/*' element={<TasksRoutes />} />
+    </Routes>
   )
 }
