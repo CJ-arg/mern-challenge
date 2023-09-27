c33571a7f1156c7ec56bf63b6e239cfc3a5632f7
unknown <janoncarlos@gmail.com>
2023-09-27 17:07:10 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">HELLO WORLD</p>
+    </>
+  );
+}
+
+export default App;
