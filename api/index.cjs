977c3be5ae584a614c3a2526076809095225a98f
unknown <janoncarlos@gmail.com>
2023-09-29 19:09:17 -0300

    refactor back to solve some bugs

---
+++ api/index.cjs
@@ -0,0 +1,13 @@
+const express = require('express');
+require("dotenv").config();
+
+
+const app = express();
+
+// app.use(express.static("../"));
+app.get("/api", (req, res) => {
+  res.json({ ok: true });
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Server running in port ${process.env.PORT}`);
+});
