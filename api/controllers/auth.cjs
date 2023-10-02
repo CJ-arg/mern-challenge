21894b701c2609c7a18a16fd016f0315c7280403
unknown <janoncarlos@gmail.com>
2023-10-02 15:18:58 -0300

    Routes res  validation

--- api/controllers/auth.cjs
+++ api/controllers/auth.cjs
@@ -1,17 +1,19 @@
  const {response} = require('express');
 
 const createUser = (req, res =  response) => {
-const {name,email,password} = req.body;
-
-  res.json({ ok: true , msg: 'register', name,email,password });
-
+  const {name,email,password} = req.body;
+if (name.length < 5) {
+ return res.status(400).json({ ok: false , msg: 'Name must be al least 5 Characters' });
+}
+  res.json({ ok: true , msg: 'register', name, email, password });
 }
 
-const loginUser = (req, res) => {
-  res.json({ ok: true , msg: 'login'});
+const loginUser = (req, res =  response) => {
+  const {email,password} = req.body;
+  res.json({ ok: true , msg: 'login' , email , password});
 }
 
-const renewToken = (req, res) => {
+const renewToken = (req, res =  response) => {
   res.json({ ok: true, msg: 'renew' });
 }
 
