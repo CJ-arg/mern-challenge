2218d18470926fb761996f1ded131256dd01ab10
unknown <janoncarlos@gmail.com>
2023-10-02 11:31:18 -0300

    Routes

---
+++ api/controllers/auth.cjs
@@ -0,0 +1,9 @@
+ const express = require('express');
+
+
+
+const createUser = (req, res = express.response) => {
+  res.json({ ok: true , msg: 'register' });
+
+}
+module.exports = {createUser,} 
--- api/routes/auth.cjs
+++ api/routes/auth.cjs
@@ -2,11 +2,10 @@
 // http://localhost:4000 + /api/auth
 
 const {Router} = require('express');
+const { createUser } = require('../controllers/auth.cjs');
 const router = Router()
 
-router.post("/new", (req, res) => {
-  res.json({ ok: true , msg: 'register' });
-});
+router.post("/new", createUser)
 router.post("/", (req, res) => {
   res.json({ ok: true , msg: 'login'});
 });
