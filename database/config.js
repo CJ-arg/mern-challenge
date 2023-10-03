5694fa630740ac9b9166a76f4ff5fe08c963f32e
unknown <janoncarlos@gmail.com>
2023-10-03 17:57:41 -0300

    database

--- database/config.js
+++ database/config.js
@@ -1,1 +1,10 @@
 import mongoose from "mongoose";
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN, {});
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al inicializar la base de Datos");
+  }
+};
