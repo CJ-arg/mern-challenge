dae441f32f4128013b5a9293b95a82d0735babcf
unknown <janoncarlos@gmail.com>
2023-10-04 10:45:43 -0300

    model user

--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,15 @@
 import { Schema, model } from "mongoose";
-
-const UserSchema = Schema({
+new Schema({
   name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
     type: String,
     require: true,
   },
