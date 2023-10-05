3aef358c6904f412e333201e33a53980dd7e131e
unknown <janoncarlos@gmail.com>
2023-10-05 15:48:37 -0300

    jwt

--- api/middlewares/validateJwt.mjs
+++ api/middlewares/validateJwt.mjs
@@ -1,7 +1,24 @@
 import { response } from "express";
-
+import jwt from "jsonwebtoken";
 export const validateJwt = (req, res, next) => {
   const token = req.header("x-token");
-  console.log(token);
+
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "No token",
+    });
+  }
+  try {
+    const payload = jwt.verify(token, process.env.SECRET_JWT_SEED);
+
+    console.log(payload);
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "No valid token",
+    });
+  }
+
   next();
 };
