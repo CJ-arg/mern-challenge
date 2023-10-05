aefa8e8ac8c629572470b00432bbf76bf742b8e0
unknown <janoncarlos@gmail.com>
2023-10-05 11:24:29 -0300

    jwt

--- api/helpers/jwt.mjs
+++ api/helpers/jwt.mjs
@@ -1,1 +1,10 @@
 import jwt from "jsonwebtoken";
+
+const generateJwt = (uid, name) => {
+  return new Promise((resolve, reject) => {
+    const payload = { uid, name };
+    jwt.sign(payload, SECRET_JWT_SEED);
+  });
+};
+
+export { generateJwt };
