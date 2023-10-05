0620db2dd0762fd699f8d98e249ae791fcd0fb91
unknown <janoncarlos@gmail.com>
2023-10-05 11:32:10 -0300

    jwt

--- api/helpers/jwt.mjs
+++ api/helpers/jwt.mjs
@@ -3,7 +3,18 @@ import jwt from "jsonwebtoken";
 const generateJwt = (uid, name) => {
   return new Promise((resolve, reject) => {
     const payload = { uid, name };
-    jwt.sign(payload, process.env.SECRET_JWT_SEED);
+    jwt.sign(
+      payload,
+      process.env.SECRET_JWT_SEED,
+      { expiresIn: "48h" },
+      (err, token) => {
+        if (err) {
+          console.log(err);
+          reject("TOKEN not generated");
+        }
+        resolve(token);
+      }
+    );
   });
 };
 
