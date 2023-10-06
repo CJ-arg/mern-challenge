d7ddc792d4bb5de307376b290339576eaf9209c2
unknown <janoncarlos@gmail.com>
2023-10-06 11:13:10 -0300

    cors

--- api/index.mjs
+++ api/index.mjs
@@ -3,8 +3,8 @@ import express from "express";
 import authRoutes from "./routes/auth.mjs";
 import dotenv from "dotenv";
 import cors from "cors";
-dotenv.config();
 
+dotenv.config();
 const app = express();
 //data base
 dbConnection();
@@ -12,13 +12,11 @@ dbConnection();
 app.use(cors());
 // public directory
 app.use(express.static("../"));
-
 // lectura parse BODY
 app.use(express.text());
 app.use(express.json());
 // Rutes
 app.use("/api/auth", authRoutes);
-
 app.listen(process.env.PORT, () => {
   console.log(`Server running in port ${process.env.PORT}`);
 });
