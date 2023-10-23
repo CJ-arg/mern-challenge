5247d8c2fc6529a4f6b68808af2fcd3be5d893e0
unknown <janoncarlos@gmail.com>
2023-10-23 10:40:41 -0300

    logout

--- src/useContext/StatusProvider.tsx
+++ src/useContext/StatusProvider.tsx
@@ -1,1 +1,16 @@
+import { ReactNode, useState } from "react";
+import { UserContext } from "./UserContext";
 
+interface ProviderProps {
+  children: ReactNode;
+}
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+  const [user, setCurrentUser] = useState({});
+
+  return (
+    <UserContext.Provider value={{ user, setCurrentUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
