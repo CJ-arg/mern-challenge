5803d255269964f3dfe7134f2b441bde194ea2a5
unknown <janoncarlos@gmail.com>
2023-10-23 13:54:09 -0300

    task FAD

--- src/components/FabAddNew.tsx
+++ src/components/FabAddNew.tsx
@@ -3,11 +3,15 @@ import { Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 const FabAddNew = () => {
+  const handleClickNew = () => {
+    console.log("modal");
+  };
   return (
     <Fab
       color="error"
       aria-label="add"
       sx={{ position: "fixed", bottom: 16, right: 16 }}
+      onClick={handleClickNew}
     >
       <AddIcon />
     </Fab>
