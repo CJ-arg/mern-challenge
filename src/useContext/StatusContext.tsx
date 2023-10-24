6ddfb737243dda904911f608c57dfc7437b18a68
unknown <janoncarlos@gmail.com>
2023-10-24 15:47:09 -0300

    context

--- src/useContext/StatusContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-interface StatusContextType {
-  status: { jwt: string } | undefined;
-  setCurrentStatus: (status: { jwt: string } | undefined) => void;
-}
-export const StatusContext = createContext<StatusContextType | undefined>(
-  undefined
-);
