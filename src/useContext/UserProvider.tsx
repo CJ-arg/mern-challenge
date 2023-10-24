39ae4cdf0be65f19552841ae6bd09bc46eadc983
unknown <janoncarlos@gmail.com>
2023-10-24 15:46:40 -0300

    context

--- src/useContext/UserProvider.tsx
+++ src/useContext/UserProvider.tsx
@@ -4,10 +4,16 @@ import { UserContext } from "./UserContext";
 interface UserProviderProps {
   children: ReactNode;
 }
+interface User {
+  id: number;
+  nickname: string;
+  email: string;
+  token: string;
+}
 
 export const UserProvider = ({ children }: UserProviderProps) => {
-  const [user, setCurrentUser] = useState({});
-  const [modalChange, setModalChange] = useState({});
+  const [user, setCurrentUser] = useState<User>();
+  const [modalChange, setModalChange] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
   return (
     <UserContext.Provider
