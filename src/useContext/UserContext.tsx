534d682921960e206d82835edc19669031927e5d
unknown <janoncarlos@gmail.com>
2023-10-27 10:22:23 -0300

    context

--- src/useContext/UserContext.tsx
+++ src/useContext/UserContext.tsx
@@ -6,17 +6,15 @@ interface UserContextType {
         id: number;
         nickname: string;
         email: string;
-         }
+      }
     | undefined;
   setCurrentUser: (
-    user:
-      | { id: number; nickname: string; email: string; }
-      | undefined
+    user: { id: number; nickname: string; email: string } | undefined
   ) => void;
   modalChange: boolean;
   setModalChange: (modalChange: boolean) => void;
-  loggedIn: boolean;
-  setLoggedIn: (modalChange: boolean) => void;
+  // loggedIn: boolean;
+  // setLoggedIn: (modalChange: boolean) => void;
 }
 
 export const UserContext = createContext<UserContextType | undefined>(
