603c2a5e72f1de8447e58bf41c8be29ebb76717f
unknown <janoncarlos@gmail.com>
2023-10-30 12:02:49 -0300

    reducer

---
+++ src/useContext/UserReducer.ts
@@ -0,0 +1,44 @@
+import { TextsState, objTextEmpty } from './TextsProvider'
+import { IText } from '../../../types/texts'
+import { LNG } from './TextsContext'
+
+type TextsActionType = 
+| {type: 'Texts', payload: IText[]}
+| {type: 'TextByLanguage', payload: IText}
+| {type: 'UpdateText', payload: IText}
+| {type: 'SetLanguage', payload: LNG}
+| {type: 'Error', payload: string}
+
+
+export const textsReducer = (state: TextsState, action: TextsActionType): TextsState => {
+    switch (action.type) {
+    case 'Texts':
+        return {
+            ...state,
+            texts: action.payloa
+        }
+    case 'TextByLanguage':
+        return {
+            ...state,
+            text: action.payload
+        }
+    case 'UpdateText':
+        return {
+            ...state,
+            text: action.payload
+        }
+    case 'SetLanguage':
+        return {
+            ...state,
+            language: action.payload
+        }
+    case 'Error':
+        return {
+            ...state,
+            message: action.payload,
+            text: objTextEmpty
+        }
+    default:
+        return state
+    }
+}
