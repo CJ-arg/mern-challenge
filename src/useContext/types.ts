21300cc8feeaec5cc77de559a4348c90bcdbdd95
unknown <janoncarlos@gmail.com>
2023-10-31 09:54:58 -0300

    reducer

---
+++ src/useContext/types.ts
@@ -0,0 +1,17 @@
+export const types = {
+  login: "[Auth] Login",
+  logout: "[Auth] Logout",
+  data: "[Data] Data",
+  modal: "[Modal] Modal",
+  user: "[User] User",
+};
+
+// TasksState,
+// data,
+// setData,
+// user,
+// setCurrentUser,
+// modalChange,
+// setModalChange,
+// loggedIn,
+// setLoggedIn,
