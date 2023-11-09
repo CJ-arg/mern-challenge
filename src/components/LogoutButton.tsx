c3e5ab2bbf9768da62713899f9d4c4f0f8e02064
unknown <janoncarlos@gmail.com>
2023-11-09 15:53:28 -0300

    logout  fixed w async function

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@mui/material";
 import { UserContext } from "../useContext/UserContext";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LogoutButton = () => {
   const { logout } = useContext(UserContext);
-  const logOut = () => {
-    logout();
+  const navigate = useNavigate();
+  const logOut = async () => {
+    await logout();
+    navigate("/auth/login");
   };
   return (
     <Button variant="contained" size="small" onClick={logOut}>
